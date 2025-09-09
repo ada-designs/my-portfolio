86b3530fc5cbc9c4fcd67b2a759e69395b26e05c
ada-designs <alex.adadesigns@gmail.com>
2025-09-09 20:36:08 +0300

    font weight adjust

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontWeight: {
+        extrabold: "900",
+      },
+    },
+  },
+  plugins: [],
+};
